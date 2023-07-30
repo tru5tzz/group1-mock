b9a7831697a961cc7bb68e178c064f69ddb5e973
toni_hieu <hieu77mcsl@gmail.com>
2023-07-30 14:45:38 +0700

    node Gateway

--- Gateway/sl_btmesh_set_uuid.c
+++ Gateway/sl_btmesh_set_uuid.c
@@ -1,31 +1,6 @@
 /***************************************************************************//**
- * @file
- * @brief Core application logic.
- *******************************************************************************
- * # License
- * <b>Copyright 2022 Silicon Laboratories Inc. www.silabs.com</b>
- *******************************************************************************
- *
- * SPDX-License-Identifier: Zlib
- *
- * The licensor of this software is Silicon Laboratories Inc.
- *
- * This software is provided 'as-is', without any express or implied
- * warranty. In no event will the authors be held liable for any damages
- * arising from the use of this software.
- *
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- *
- * 1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software. If you use this software
- *    in a product, an acknowledgment in the product documentation would be
- *    appreciated but is not required.
- * 2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any source distribution.
- *
+ * @file  sl_btmesh_set_uuid.c
+ * @brief function set uuid for device.
  ******************************************************************************/
 #include "em_common.h"
 #include "app_assert.h"
@@ -36,14 +11,30 @@
 #include "sl_btmesh_api.h"
 #include "app_log.h"
 
+/*******************************************************************************
+ *******************************   DEFINES   ***********************************
+ ******************************************************************************/
 #define LENGTH_COMPANY_ID       6
 #define LENGTH_UUID             16
 #define CHAR_MIN                0
 #define CHAR_MAX                255
 
+/*******************************************************************************
+ *******************************   LOCAL VARIABLES   ***************************
+ ******************************************************************************/
 static  uuid_128 my_uuid_device;
 static  uint8_t company_uuid[LENGTH_COMPANY_ID]={0,0,2,255,0,2};
 
+/*******************************************************************************
+ *******************************   GLOBAL FUNCTION   ***************************
+ ******************************************************************************/
+
+
+/**************************************************************************//**
+ *Function set uuid for device
+ *
+ * @param[in] evt Event coming from the Bluetooth Mesh stack.
+ *****************************************************************************/
 void sl_btmesh_set_my_uuid(sl_bt_msg_t *evt)
 {
   sl_status_t sc;
@@ -51,17 +42,25 @@ void sl_btmesh_set_my_uuid(sl_bt_msg_t *evt)
 
   switch (SL_BT_MSG_ID(evt->header)) {
       case sl_bt_evt_system_boot_id:
+        // get uuid device
         sc = sl_btmesh_node_get_uuid(&my_uuid_device);
-        if(sc != SL_STATUS_OK) {
+        if(sc != SL_STATUS_OK)
+        {
             /* Something went wrong */
             app_log("sl_btmesh_node_get_uuid: failed 0x%.2lx\r\n", sc);
-        } else {
+        }
+        else
+        {
+            // set uuid for device
             app_log("Success,sl_btmesh_node_get_uuid\n");
-            for(index=0;index<LENGTH_UUID;index++){
-                if(index<LENGTH_COMPANY_ID){
+            for(index=0;index<LENGTH_UUID;index++)
+            {
+                if(index<LENGTH_COMPANY_ID)
+                {
                     my_uuid_device.data[index]=company_uuid[index];
                 }
-                else{
+                else
+                {
                     my_uuid_device.data[index]= CHAR_MIN + rand() % (CHAR_MAX-CHAR_MIN+1);
                 }
 
@@ -71,7 +70,9 @@ void sl_btmesh_set_my_uuid(sl_bt_msg_t *evt)
         if(sc != SL_STATUS_OK) {
             /* Something went wrong */
             app_log("sl_btmesh_node_set_uuid: failed 0x%.2lx\r\n", sc);
-        } else {
+        }
+        else
+        {
             app_log("Success,sl_btmesh_node_set_uuid\n");
         }
         break;
--- Gateway/sl_btmesh_set_uuid.h
+++ Gateway/sl_btmesh_set_uuid.h
@@ -1,13 +1,16 @@
-/*
- * sl_btmesh_set_uuid.h
- *
- *  Created on: Jul 26, 2023
- *      Author: Admin
- */
+/***************************************************************************//**
+ * @file  sl_btmesh_set_uuid.h
+ * @brief prototype function set uuid for device.
+ ******************************************************************************/
 
 #ifndef SL_BTMESH_SET_UUID_H_
 #define SL_BTMESH_SET_UUID_H_
 
+/**************************************************************************//**
+ *Function set uuid for device
+ *
+ * @param[in] evt Event coming from the Bluetooth Mesh stack.
+ *****************************************************************************/
 void sl_btmesh_set_my_uuid(sl_bt_msg_t *evt);
 
 #endif /* SL_BTMESH_SET_UUID_H_ */
